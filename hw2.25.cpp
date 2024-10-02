3022a6fd90852773df78f86a6dfdb7b4ac81fa5c
cby0221 <liuzhihao240@gmail.com>
2024-10-02 20:30:57 +0800

    Add files via upload

---
+++ hw2.25.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	printf("     ccccccc           \n");
+	printf("   c         c         \n");
+	printf("  c            c       \n");
+	printf("  c            c       \n");
+	printf("  c           c        \n");
+	printf("    c        c         \n");
+	printf("                       \n");
+	printf("    BBBBBBBBBBBBBBB    \n");
+	printf("    B      B      B    \n");
+	printf("    B      B      B    \n");
+	printf("     B    B B    B     \n");
+	printf("       BB     BB       \n");
+	printf("                    YY \n");
+	printf("                 YY    \n");
+	printf("              YY       \n");
+	printf(" YYYYYYYYYYYY          \n");
+	printf("              YY       \n");
+	printf("                 YY    \n");
+	printf("                    YY \n");
+}
